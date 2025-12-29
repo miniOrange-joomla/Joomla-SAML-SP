b7ce13ee1390ebc250eba62f9b6f7c9239b078b4
miniOrange-joomla <215139780+miniOrange-joomla@users.noreply.github.com>
2025-12-29 21:58:42 +0530

    Add files via upload

---
+++ Login with Azure AD SAML SP - ADFS Single Sign-On - SAML SSO/miniorange-saml-sso-for-joomla/com_miniorange_saml/administrator/helpers/BasicEnum.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * @package     Joomla.Component	
+ * @subpackage  com_miniorange_saml
+ *
+ * @author      miniOrange Security Software Pvt. Ltd.
+ * @copyright   Copyright (C) 2015 miniOrange (https://www.miniorange.com)
+ * @license     GNU General Public License version 3; see LICENSE.txt
+ * @contact     info@xecurify.com
+ */
+
+
+defined('_JEXEC') or die;
+
+abstract class BasicEnum {
+	private static $constCacheArray = NULL;
+
+	public static function getConstants() {
+		if (self::$constCacheArray == NULL) {
+			self::$constCacheArray = array();
+		}
+		$calledClass = get_called_class();
+		if (!array_key_exists($calledClass, self::$constCacheArray)) {
+			$reflect = new ReflectionClass($calledClass);
+			self::$constCacheArray[$calledClass] = $reflect->getConstants();
+		}
+		return self::$constCacheArray[$calledClass];
+	}
+
+	public static function isValidName($name, $strict = false) {
+		$constants = self::getConstants();
+
+		if ($strict) {
+			return array_key_exists($name, $constants);
+		}
+
+		$keys = array_map('strtolower', array_keys($constants));
+		return in_array(strtolower($name), $keys);
+	}
+
+	public static function isValidValue($value, $strict = true) {
+		$values = array_values(self::getConstants());
+		return in_array($value, $values, $strict);
+	}
+}
