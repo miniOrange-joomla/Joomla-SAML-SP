d9156e3745976d37ea660e3fb507773ccd98deae
miniOrange-joomla <215139780+miniOrange-joomla@users.noreply.github.com>
2025-12-29 22:09:52 +0530

    Add files via upload

---
+++ Login with Google Apps SAML SP - GAPPS Single Sign-On - SAML SSO/miniorange-saml-sso-for-joomla/plg_system_samlredirect/my_script.php
@@ -0,0 +1,91 @@
+<?php
+/**
+ * @package     Joomla.Plugin	
+ * @subpackage  plg_system_samlredirect
+ *
+ * @author      miniOrange Security Software Pvt. Ltd.
+ * @copyright   Copyright (C) 2015 miniOrange (https://www.miniorange.com)
+ * @license     GNU General Public License version 3; see LICENSE.txt
+ * @contact     info@xecurify.com
+ */
+
+defined('_JEXEC') or die('Restricted access');
+use Joomla\CMS\Factory;
+class plgSystemSamlredirectInstallerScript
+{
+    /**
+     * This method is called after a component is installed.
+     *
+     * @param  \stdClass $parent - Parent object calling this method.
+     *
+     * @return void
+     */
+    public function install($parent) 
+    {
+
+          $db  = Factory::getDbo();
+          $query1 = $db->getQuery(true);
+          $query1->update('#__extensions');
+          $query1->set($db->quoteName('enabled') . ' = 1');
+          $query1->where($db->quoteName('element') . ' = ' . $db->quote('samlredirect'));
+          $query1->where($db->quoteName('type') . ' = ' . $db->quote('plugin'));
+          $db->setQuery($query1);
+          $db->execute();
+            
+    }
+
+    /**
+     * This method is called after a component is uninstalled.
+     *
+     * @param  \stdClass $parent - Parent object calling this method.
+     *
+     * @return void
+     */
+    public function uninstall($parent) 
+    {
+    }
+
+    /**
+     * This method is called after a component is updated.
+     *
+     * @param  \stdClass $parent - Parent object calling object.
+     *
+     * @return void
+     */
+    public function update($parent) 
+    {
+       
+    }
+
+    /**
+     * Runs just before any installation action is performed on the component.
+     * Verifications and pre-requisites should run in this function.
+     *
+     * @param  string    $type   - Type of PreFlight action. Possible values are:
+     *                           - * install
+     *                           - * update
+     *                           - * discover_install
+     * @param  \stdClass $parent - Parent object calling object.
+     *
+     * @return void
+     */
+    public function preflight($type, $parent) 
+    {
+    }
+
+    /**
+     * Runs right after any installation action is performed on the component.
+     *
+     * @param  string    $type   - Type of PostFlight action. Possible values are:
+     *                           - * install
+     *                           - * update
+     *                           - * discover_install
+     * @param  \stdClass $parent - Parent object calling object.
+     *
+     * @return void
+     */
+    function postflight($type, $parent) 
+    {
+    }
+
+}
